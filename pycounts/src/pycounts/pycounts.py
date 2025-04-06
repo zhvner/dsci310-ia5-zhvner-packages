4e2400c83f9f500c7152eb68a6ff87eb0bcc63e4
Zhanerke Zhumash <szhanerz@gmail.com>
2025-04-05 18:36:25 -0700

    docs: created docstrings for package functions

--- pycounts/src/pycounts/pycounts.py
+++ pycounts/src/pycounts/pycounts.py
@@ -2,13 +2,44 @@ from collections import Counter
 from string import punctuation
 
 def load_text(input_file):
-        """Load text from a text file and return as a string."""
+        """Load text from a text file and return as a string.
+
+        Parameters
+        ----------
+        input_file : str
+            Path to text file.
+
+        Returns
+        -------
+        str
+            Text file contents.
+
+        Examples
+        --------
+        >>> load_text("text.txt")
+        """
         with open(input_file, "r") as file:
             text = file.read()
         return text
 
 def clean_text(text):
-        """Lowercase and remove punctuation from a string."""
+        """Lowercase and remove punctuation from a string.
+
+        Parameters
+        ----------
+        text : str
+            Text to clean.
+
+        Returns
+        -------
+        str
+            Cleaned text.
+
+        Examples
+        --------
+        >>> clean_text("Early optimization is the root of all evil!")
+        'early optimization is the root of all evil'
+        """
         text = text.lower()
         for p in punctuation:
             text = text.replace(p, "")
