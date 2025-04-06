394dc061fe6b4210812b088c93780ed8649b2744
Zhanerke Zhumash <szhanerz@gmail.com>
2025-04-05 18:17:40 -0700

    added docstrings

--- pycounts/src/pycounts/pycounts.py
+++ pycounts/src/pycounts/pycounts.py
@@ -15,7 +15,25 @@ def clean_text(text):
         return text
 
 def count_words(input_file):
-        """Count unique words in a string."""
+        """Count words in a text file.
+
+        Words are made lowercase and punctuation is removed 
+        before counting.
+
+        Parameters
+        ----------
+        input_file : str
+            Path to text file.
+
+        Returns
+        -------
+        collections.Counter
+            dict-like object where keys are words and values are counts.
+
+        Examples
+        --------
+        >>>count_words("text.txt")
+        """
         text = load_text(input_file)
         text = clean_text(text)
         words = text.split()
